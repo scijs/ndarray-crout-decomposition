805677771c9f731491743e94e56a71cc744363fb
James Halliday <mail@substack.net>
2014-05-25 18:47:53 -0700

    passing test

--- test/2d.js
+++ test/2d.js
@@ -1,16 +1,19 @@
 var test = require('tape');
 var ndarray = require('ndarray');
+var zeros = require('zeros');
 var lu = require('../');
 
 test('2d', function (t) {
-    var dL = Array(4);
-    var dU = Array(4);
-    var L = ndarray(dL, [ 2, 2 ]);
-    var U = ndarray(dU, [ 2, 2 ]);
+    var L = zeros([ 2, 2 ]);
+    var U = zeros([ 2, 2 ]);
     var A = ndarray([ 4, 3, 6, 3 ], [ 2, 2 ]);
     
     lu(A, L, U);
-    t.deepEqual(dU, [ 1, 0, 1.5, 1 ], 'U');
-    t.deepEqual(dL, [ 4, 3, 0, -1.5 ], 'L');
+    t.deepEqual(toA(U.data), [ 1, 0, 1.5, 1 ], 'U');
+    t.deepEqual(toA(L.data), [ 4, 3, 0, -1.5 ], 'L');
     t.end();
 });
+
+function toA (x) {
+    return [].slice.call(x);
+}
